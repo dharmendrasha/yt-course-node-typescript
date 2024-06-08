4885b9dbd34af63bc1b781936f5826ca73d1845b
dharmendrasha <dharmendra.soni@scaletech.xyz>
2024-06-08 22:22:34 +0530

    chore: added delete endpoint

--- src/routes/task/delete.ts
+++ src/routes/task/delete.ts
@@ -1,3 +1,13 @@
 import { Request, Response } from "express";
+import { TaskSchema } from "schema/task.schema";
 
-export async function Delete(req: Request, res: Response){}
+
+export async function Delete(req: Request<{id?: string}>, res: Response){
+
+    if(req.params.id){
+        return res.json({message: 'not found'}).status(404)
+    }
+
+
+    TaskSchema.deleteOne({id: req.params.id})
+}
